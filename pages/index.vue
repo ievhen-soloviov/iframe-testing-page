e6da04cb58ec7b5194b11ba19337b41072c30193
Eugene Soloviov <ievhen.soloviov@gmail.com>
2020-02-06 20:50:12 +0200

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="container">
-    <div class="columns">
+    <form @submit.prevent="generate" class="columns is-multiline">
       <div class="column field">
         <label class="label">Enter background color hex:</label>
         <div class="control">
@@ -17,10 +17,11 @@
           <label class="label">Enter your iframe URL:</label>
           <div class="control">
             <input
+              ref="url"
               class="input"
               type="text"
               placeholder="iframe URL"
-              v-model="url"
+              required
             >
           </div>
       </div>
@@ -35,9 +36,12 @@
             >
           </div>
       </div> -->
-    </div>
+      <div class="column is-12">
+        <button class="button is-primary is-rounded" :class="{ 'is-loading': loading }">Generate iframe</button>
+      </div>
+    </form>
     <div v-if="url" class="iframe-container">
-      <script :id="url.match(/(SA-)\w+/g)" :src="url"></script>
+      <script :id="iframeID(url)" :src="url" />
       <!-- <script id="SA-NA1PVPYNKCAJZ" src="https://sunrise-go-feature-sn-506-essay-popup-advxqrwbca-uc.a.run.app/SA-NA1PVPYNKCAJZ/frm.js"></script> -->
     </div>
   </main>
@@ -49,12 +53,21 @@ export default {
     return {
       url: null,
       bg: null,
-      // width: '800px'
+      loading: false
     }
   },
   methods: {
     updateBg() {
       document.documentElement.style.setProperty('--bg-color', this.bg);
+    },
+    generate() {
+      // console.log(this.$refs.url);
+      this.url = this.$refs.url.value;
+      this.loading = true;
+      document.getElementById(iframeID(this.url)).onload = () => this.loading = false;
+    },
+    iframeID(url) {
+      url.match(/(SA-)\w+/g);
     }
   }
 }
@@ -75,5 +88,9 @@ main {
 }
 .iframe-container > iframe {
   height: 100%;
+}
+.button {
+  display: flex;
+  margin: auto;
 }
 </style>
